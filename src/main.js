0ec9d6895c7856dffc9fbf50942ae6af7c8d63d2
zouyang2015 <86022310@qq.com>
2018-07-02 17:50:14 +0800

    me

--- src/main.js
+++ src/main.js
@@ -11,12 +11,12 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: [],
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'mpvue',
-      navigationBarTextStyle: 'black'
+      backgroundTextStyle: 'Light',
+      navigationBarBackgroundColor: '#4F7DDD',
+      navigationBarTitleText: '图书馆',
+      navigationBarTextStyle: 'white'
     }
   }
 }
