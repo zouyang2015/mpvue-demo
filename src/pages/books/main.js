278ec96cfa08a284941ad25af23f1ba00ddffe08
zouyang2015 <86022310@qq.com>
2018-07-02 18:01:14 +0800

    me

---
+++ src/pages/books/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Me from './books'
+
+const app = new Vue(Me)
+app.$mount()
---
+++ src/pages/comments/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Me from './comments'
+
+const app = new Vue(Me)
+app.$mount()
